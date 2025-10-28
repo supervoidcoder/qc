5e0e1d4bba2c5ee697379854e4e04b70b2d96814
supervoidcoder <voidcoder@outlook.com>
2025-10-27 20:34:54 -0400

    im so anxious about my code breaking this is a small commit loloolloololoo

--- README.md
+++ README.md
@@ -3,3 +3,37 @@ It's just a silly project I made to help map existing commands in Windows to a s
 
 uh, you wanted mac? why are you using mac?
 
+## docs i guess
+
+### qc.cpp
+this is the main file which like actually is the app
+uh yeah
+it has quirky erorr handling inside functions
+```error code meanings:
+1 : failed to read config
+2: failed to create config
+3: failed to append new mapping to config
+4: failed to execute new mapping from config
+67: success
+why is 67 success instead of 0? well you see the fact 67 is a very funny meme
+```
+uh
+
+### config.txt
+
+so basically this isn't inside the dev built thing
+once you install the thing in your program files that's the thing that saves your aliases 
+
+### build.py
+it builds the thing using iexpress.exe
+why? because 
+
+### license
+    i usually use gplv3 for my projects
+    but this is like a super small project anyways so 
+    im giving it up as MIT
+    basically allow anyone to use it as long as I get credited
+    nothing much
+
+
+
--- src/qc.cpp
+++ src/qc.cpp
@@ -4,5 +4,52 @@
 #include <vector>
 #include <map>
 #include <filesystem>
+// *gasp* why are you importing foreign things in your program?
+// think about the TARRIFS!
+namespace fs = std::filesystem; //  WHAT U SAY USING NAMESPACES IS BAD PRACTICE LECTHQNWJETHPQ5
+
+/* 
+error code meanings:
+1 : failed to read config
+2: failed to create config
+3: failed to append new mapping to config
+4: failed to execute new mapping from config
+67: success
+why is 67 success instead of 0? well you see the fact 67 is a very funny meme
+*/
+
+class configManage {
+public: // good thing c++ doesn't scold me for indentation *cough* python
+// use int to make my own fancy schmancy error codes
+int createConfig() {
+
+    fs::path filePath = "config.txt";
+    if (!fs::exists(filePath)) {
+        std::ofstream outfile(filePath);
+        if (outfile) {
+            outfile << "<header_start> hi! this is the configuration file for the QC Quick Command tool. please don't manually edit if you don't know what you're doing! :) <header_end>\n";
+            // quirky headers because it's scrumpdilitiously funny to parse
+            std::cout << "Configuration file created.\n";
+            outfile.close();
+            return 67; 
+            // the temptation to return 67 instead is crazy
+            // future me: i did it anyways
+        } else {
+            std::cerr << "Error: Could not create the file.\n";
+            return 1;
+        }
+         
+    }
 
 
+
+}
+int addConfig() {
+
+
+
+}
+
+};
+// man doing this is such a refreshing experience from vibe coding
+// don't vibe code it will cripple you 
